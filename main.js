97a743918f55e0b3ae12f721f7a199a8682306cf
PrateekGithub786 <75919841+PrateekGithub786@users.noreply.github.com>
2021-09-14 16:34:12 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+video = "";
+check = "";
+
+function preload(){
+    video = createVideo("video.mp4");
+}
+
+function setup(){
+    canvas = createCanvas(300, 300);
+    canvas.center();
+    video.hide();
+}
+
+function start(){
+    objectDetector = ml5.objectDetector("cocossd", modelLoaded);
+    document.getElementById("status").innerHTML = "Status : Detecting Objects";
+}
+
+function modelLoaded(){
+    console.log("Model Is Loaded!");
+    check = true;
+    video.loop();
+    video.speed(1);
+    video.volume(0);
+}
